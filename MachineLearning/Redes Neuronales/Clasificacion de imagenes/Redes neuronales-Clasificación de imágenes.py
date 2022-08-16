c31399b2b4787ef90b19152b739f0d40f02bd5f7
Luis Valencia <77359338+stalin246@users.noreply.github.com>
2022-08-15 21:22:47 -0500

    Add files via upload

---
+++ MachineLearning/Redes Neuronales/Clasificacion de imagenes/Redes neuronales-Clasificación de imágenes.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+# Integrantes: Eduardo Farinango, Luis Valencia
+
+# Instalamos tensorflow, keras e importamos las libreria 
+import numpy as np
+import os
+import re
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import keras
+from keras.utils import to_categorical
+from tensorflow.python.keras.models import Input
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import BatchNormalization
+
+
+# In[40]:
+
+
+# Cargamos las imagenes del directorio "sportimages de forma local"
+dirname = os.path.join(os.getcwd(), 'sportimages')
+imgpath = dirname + os.sep 
+
+images = []
+directories = []
+dircount = []
+prevRoot=''
+cant=0
+
+print("leyendo imagenes de ",imgpath)
+
+for root, dirnames, filenames in os.walk(imgpath):
+    for filename in filenames:
+        if re.search("\.(jpg|jpeg|png|bmp|tiff)$", filename):
+            cant=cant+1
+            filepath = os.path.join(root, filename)
+            image = plt.imread(filepath)
+            images.append(image)
+            b = "Leyendo..." + str(cant)
+            print (b, end="\r")
+            if prevRoot !=root:
+                print(root, cant)
+                prevRoot=root
+                directories.append(root)
+                dircount.append(cant)
+                cant=0
+dircount.append(cant)
+
+dircount = dircount[1:]
+dircount[0]=dircount[0]+1
+print('Directorios leidos:',len(directories))
+print("Imagenes en cada directorio", dircount)
+print('suma Total de imagenes en subdirs:',sum(dircount))
+
+
+# In[41]:
+
+
+# Creamos las etiquetas y las clases
+labels=[]
+indice=0
+for cantidad in dircount:
+    for i in range(cantidad):
+        labels.append(indice)
+    indice=indice+1
+print("Cantidad etiquetas creadas: ",len(labels))
+ 
+deportes=[]
+indice=0
+for directorio in directories:
+    name = directorio.split(os.sep)
+    print(indice , name[len(name)-1])
+    deportes.append(name[len(name)-1])
+    indice=indice+1
+ 
+y = np.array(labels)
+X = np.array(images, dtype=np.uint8) #convierto de lista a numpy
+ 
+# Find the unique numbers from the train labels
+classes = np.unique(y)
+nClasses = len(classes)
+print('Total number of outputs : ', nClasses)
+print('Output classes : ', classes)
+
+
+# In[46]:
+
+
+# importamos LeakyRelo de keras
+from keras.layers import LeakyReLU
+
+
+# In[47]:
+
+
+#Mezclar todo y crear los grupos de entrenamiento y testing
+train_X,test_X,train_Y,test_Y = train_test_split(X,y,test_size=0.2)
+print('Training data shape : ', train_X.shape, train_Y.shape)
+print('Testing data shape : ', test_X.shape, test_Y.shape)
+ 
+train_X = train_X.astype('float32')
+test_X = test_X.astype('float32')
+train_X = train_X / 255.
+test_X = test_X / 255.
+
+# Cramos sets de entrenamiento y test
+# Change the labels from categorical to one-hot encoding
+train_Y_one_hot = to_categorical(train_Y)
+test_Y_one_hot = to_categorical(test_Y)
+ 
+# Display the change for category label using one-hot encoding
+print('Original label:', train_Y[0])
+print('After conversion to one-hot:', train_Y_one_hot[0])
+#Mezclar todo y crear los grupos de entrenamiento y testing
+train_X,valid_X,train_label,valid_label = train_test_split(train_X, train_Y_one_hot, test_size=0.2, random_state=13)
+ 
+print(train_X.shape,valid_X.shape,train_label.shape,valid_label.shape)
+
+
+# In[48]:
+
+
+# Creamos el modelo
+INIT_LR = 1e-3
+epochs = 6
+batch_size = 64
+ 
+sport_model = Sequential()
+sport_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',padding='same',input_shape=(21,28,3)))
+sport_model.add(LeakyReLU(alpha=0.1))
+sport_model.add(MaxPooling2D((2, 2),padding='same'))
+sport_model.add(Dropout(0.5))
+ 
+sport_model.add(Flatten())
+sport_model.add(Dense(32, activation='linear'))
+sport_model.add(LeakyReLU(alpha=0.1))
+sport_model.add(Dropout(0.5)) 
+sport_model.add(Dense(nClasses, activation='softmax'))
+ 
+sport_model.summary()
+ 
+sport_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adagrad(lr=INIT_LR, decay=INIT_LR / 100),metrics=['accuracy'])
+
+
+# In[50]:
+
+
+sport_train_dropout = sport_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+ 
+# guardamos la red, para reutilizarla en el futuro, sin tener que volver a entrenar
+sport_model.save("sports_mnist.h5py")
+
+
+# In[53]:
+
+
+# EVALUAMOS LA RED
+test_eval = sport_model.evaluate(test_X, test_Y_one_hot, verbose=1)
+ 
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+
+# In[56]:
+
+
+predicted_classes2 = sport_model.predict(test_X)
+predicted_classes=[]
+for predicted_sport in predicted_classes2:
+    predicted_classes.append(predicted_sport.tolist().index(max(predicted_sport)))
+predicted_classes=np.array(predicted_classes)
+
+predicted_classes.shape, test_Y.shape
+
+
+# In[57]:
+
+
+# APRENDEMOS QUE ERRORES MEJORAR
+correct = np.where(predicted_classes==test_Y)[0]
+print("Found %d correct labels" % len(correct))
+for i, correct in enumerate(correct[0:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(test_X[correct].reshape(21,28,3), cmap='gray', interpolation='none')
+    plt.title("{}, {}".format(deportes[predicted_classes[correct]],
+                                                    deportes[test_Y[correct]]))
+
+    plt.tight_layout()
+
+
+# In[58]:
+
+
+incorrect = np.where(predicted_classes!=test_Y)[0]
+print("Found %d incorrect labels" % len(incorrect))
+for i, incorrect in enumerate(incorrect[0:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(test_X[incorrect].reshape(21,28,3), cmap='gray', interpolation='none')
+    plt.title("{}, {}".format(deportes[predicted_classes[incorrect]],
+                                                    deportes[test_Y[incorrect]]))
+    plt.tight_layout()
+
+
+# In[59]:
+
+
+target_names = ["Class {}".format(i) for i in range(nClasses)]
+print(classification_report(test_Y, predicted_classes, target_names=target_names))
+
+
+# In[ ]:
+
+
+
+
